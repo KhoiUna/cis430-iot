a00878ef4bf605104b0018b7a96574454bf425c6
KhoiNguyen <thorwaitson@gmail.com>
2022-02-21 03:40:24 -0600

    update config.js

--- src/scripts/config/config.js
+++ src/scripts/config/config.js
@@ -1,5 +1,8 @@
 const config = {
-  origin: location.protocol === "http:" ? "http://localhost/dashboard/php" : "",
+  origin:
+    location.protocol === "http:"
+      ? "http://localhost/dashboard/php"
+      : "https://cis.meetlearn.net/cis430-iot/php",
 };
 
 export default config;
