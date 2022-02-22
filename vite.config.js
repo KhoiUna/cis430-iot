5dd9b98c794ec3cf7b86dcc47721d79cf7396b72
KhoiNguyen <thorwaitson@gmail.com>
2022-02-21 19:49:54 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "./",
   build: {
     manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "device.html"),
+      },
+    },
   },
   css: {
     plugins: {
