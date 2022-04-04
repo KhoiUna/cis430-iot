3319e38467f5f9eade2d1c98ddc667f9f08f41c5
KhoiNguyen <thorwaitson@gmail.com>
2022-04-04 14:58:23 -0500

    update php files

---
+++ php/config.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$servername = getenv('SERVERNAME');
+$username = getenv('USERNAME');
+$password = getenv('PASSWORD');
+$dbname = getenv('DBNAME');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo json_encode(array(
+        "error" => "Connection failed",
+        "success" => false
+    ));
+}
+?>
--- php/getDevices.php
+++ php/getDevices.php
@@ -1,22 +1,5 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
-
-$servername = getenv('SERVERNAME');
-$username = getenv('USERNAME');
-$password = getenv('PASSWORD');
-$dbname = getenv('DBNAME');
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo json_encode(array(
-        "error" => "Connection failed",
-        "success" => false
-    ));
-}
+require_once 'config.php';
 
 $sql = "SELECT DeviceID, `Name` FROM Device;";
 // echo $sql;
