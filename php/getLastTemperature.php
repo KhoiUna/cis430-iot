de709fe3b03df9c1d130eff0029ba2f5de6eff4e
KhoiNguyen <thorwaitson@gmail.com>
2022-04-09 21:47:32 -0500

    update getLastTemperature.php

--- php/getLastTemperature.php
+++ php/getLastTemperature.php
@@ -3,16 +3,20 @@ require_once 'config.php';
 
 $deviceId = $_GET["deviceID"];
 if(isset($deviceId)) {
-    $sql = "SELECT * FROM Temperature WHERE DeviceID ='" . $deviceId . "' ORDER BY Timestamp DESC LIMIT 1;";
+    $sql = "SELECT `Timestamp`, DeviceTemp FROM Temperature WHERE DeviceID ='" . $deviceId . "' ORDER BY Timestamp DESC LIMIT 1;";
     // echo $sql;
 
     $result = $conn->query($sql)->fetch_array();
 
+    $timestamp= $result['Timestamp'];
     $deviceTemp = $result['DeviceTemp'];
 
     echo json_encode(array(
         "error" => false,
-        "success" => (float) $deviceTemp
+        "success" => array(
+            "timestamp" => $timestamp,
+            "temperature" => (float) $deviceTemp
+        )
     ));
 } else {
     echo json_encode(array(
